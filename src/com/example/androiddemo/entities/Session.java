daeafba27e5cf8890cce2de71f68ae6cbb7f0e0e
Jonathan Perry <j.m.perry@hotmail.co.uk>
2013-05-30 20:14:12 +0100

    Implemented all ui views. Need to work on app logic next. Build is stable... I think...

---
+++ src/com/example/androiddemo/entities/Session.java
@@ -0,0 +1,30 @@
+package com.example.androiddemo.entities;
+
+import java.util.Date;
+
+public class Session {
+
+	private long time;
+	private Date dt;
+	
+	public Session() {
+		
+	}
+	
+	public void setTime(long time) {
+		this.time = time;
+	}
+	
+	public long getTime() {
+		return time;
+	}
+	
+	public void setDateTime(Date dt) {
+		this.dt = dt;
+	}
+	
+	public Date getDateTime() {
+		return dt;
+	}
+	
+}
