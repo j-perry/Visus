7aecff46726dad977124d6f7274b4007ddb7acaa
Jonathan Perry <j.m.perry@hotmail.co.uk>
2013-07-27 20:09:35 +0100

    Main Menu (Menu Inflater)
    Added items to the settings sub-menu.

--- gen/com/visus/R.java
+++ gen/com/visus/R.java
@@ -54,8 +54,10 @@ public final class R {
         public static final int first_name=0x7f090003;
         public static final int gender=0x7f090004;
         public static final int menu_about=0x7f090009;
-        public static final int menu_settings=0x7f09000b;
-        public static final int new_session_menu=0x7f09000a;
+        public static final int menu_account=0x7f09000b;
+        public static final int menu_sessions=0x7f09000a;
+        public static final int menu_settings=0x7f09000c;
+        public static final int new_session_menu=0x7f09000d;
         public static final int no_sessions=0x7f090007;
         public static final int register=0x7f090006;
         public static final int timer=0x7f090000;
@@ -78,41 +80,45 @@ public final class R {
         public static final int activity_view_settings=0x7f080004;
     }
     public static final class string {
-        public static final int account_delete=0x7f050018;
-        public static final int account_header=0x7f050015;
-        public static final int account_modify=0x7f050017;
-        public static final int account_reset=0x7f050016;
+        public static final int account_delete=0x7f05001a;
+        public static final int account_header=0x7f050017;
+        public static final int account_modify=0x7f050019;
+        public static final int account_reset=0x7f050018;
         public static final int app_name=0x7f050000;
-        public static final int default_sessions_no=0x7f050011;
-        public static final int gender_prompt=0x7f050009;
+        public static final int default_sessions_no=0x7f050013;
+        public static final int gender_prompt=0x7f05000b;
         public static final int hello_world=0x7f050001;
-        public static final int hint_age=0x7f05000a;
+        public static final int hint_age=0x7f05000c;
         /**  New User 
          */
-        public static final int hint_firstname=0x7f050008;
+        public static final int hint_firstname=0x7f05000a;
+        /**  Main Activity Menu 
+         */
         public static final int menu_about=0x7f050002;
+        public static final int menu_account=0x7f050003;
+        public static final int menu_sessions=0x7f050004;
         /**  Settings 
          */
-        public static final int menu_settings=0x7f050010;
-        public static final int normal_theme=0x7f050013;
+        public static final int menu_settings=0x7f050012;
+        public static final int normal_theme=0x7f050015;
         /**  Previous Session 
          */
-        public static final int prev_session=0x7f05000f;
-        public static final int register=0x7f05000b;
-        public static final int themes_header=0x7f050012;
-        public static final int title_activity_new_session=0x7f050004;
+        public static final int prev_session=0x7f050011;
+        public static final int register=0x7f05000d;
+        public static final int themes_header=0x7f050014;
+        public static final int title_activity_new_session=0x7f050006;
         /**  Menu options 
          */
-        public static final int title_activity_new_user=0x7f050003;
-        public static final int title_activity_prev_sessions=0x7f050005;
-        public static final int title_activity_user_accounts=0x7f050007;
-        public static final int title_activity_view_settings=0x7f050006;
-        public static final int title_end_session=0x7f05000e;
+        public static final int title_activity_new_user=0x7f050005;
+        public static final int title_activity_prev_sessions=0x7f050007;
+        public static final int title_activity_user_accounts=0x7f050009;
+        public static final int title_activity_view_settings=0x7f050008;
+        public static final int title_end_session=0x7f050010;
         /**  New Session 
          */
-        public static final int title_new_session=0x7f05000c;
-        public static final int title_start_session=0x7f05000d;
-        public static final int weather_theme=0x7f050014;
+        public static final int title_new_session=0x7f05000e;
+        public static final int title_start_session=0x7f05000f;
+        public static final int weather_theme=0x7f050016;
     }
     public static final class style {
         /** 
