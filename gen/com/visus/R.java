4686ebfebdc25a1c604767abaa71ba372b7782a1
Jonathan Perry <j.m.perry@hotmail.co.uk>
2013-12-21 13:59:44 +0000

    Binary files

--- gen/com/visus/R.java
+++ gen/com/visus/R.java
@@ -193,10 +193,11 @@ public final class R {
         public static final int layout_sessions_item=0x7f030018;
         public static final int layout_sessions_items=0x7f030019;
         public static final int layout_settings_group_item=0x7f03001a;
-        public static final int settings_spinner_gender_layout=0x7f03001b;
-        public static final int settings_spinner_gender_list_layout=0x7f03001c;
-        public static final int sign_up_spinner_gender_layout=0x7f03001d;
-        public static final int sign_up_spinner_gender_list_layout=0x7f03001e;
+        public static final int settings_activities_layout_item=0x7f03001b;
+        public static final int settings_spinner_gender_layout=0x7f03001c;
+        public static final int settings_spinner_gender_list_layout=0x7f03001d;
+        public static final int sign_up_spinner_gender_layout=0x7f03001e;
+        public static final int sign_up_spinner_gender_list_layout=0x7f03001f;
     }
     public static final class menu {
         public static final int activity_main=0x7f080000;
