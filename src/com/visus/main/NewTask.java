ffc97e1b6b1ed0a75e5aebcaf20ce3a5cc0858ee
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-09-18 12:31:14 +0100

    Added content views for both newly added controllers

--- src/com/visus/main/NewTask.java
+++ src/com/visus/main/NewTask.java
@@ -1,5 +1,7 @@
 package com.visus.main;
 
+import com.visus.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -8,6 +10,8 @@ public class NewTask extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_new_task);
 	}
 	
 }
--- src/com/visus/main/Tasks.java
+++ src/com/visus/main/Tasks.java
@@ -1,11 +1,17 @@
 package com.visus.main;
 
+import com.visus.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Tasks extends Activity {
 
-	public Tasks() {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		
+		setContentView(R.layout.activity_tasks);
 	}
 	
 	
