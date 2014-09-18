1ae957495d8f41dc37e33502b40428af7a1cdab0
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-09-18 12:03:23 +0100

    Amended string resource. Added new string menu resources to activity_main.xml

--- gen/com/visus/R.java
+++ gen/com/visus/R.java
@@ -213,9 +213,11 @@ containing a value of this type.
         public static final int main_menu_latest_activities_no_sessions_total=0x7f080026;
         public static final int main_menu_no_activities=0x7f080023;
         public static final int main_menu_pager=0x7f080009;
-        public static final int menu_about=0x7f080046;
+        public static final int menu__new_task=0x7f080046;
+        public static final int menu_about=0x7f080048;
         public static final int menu_sessions=0x7f080044;
-        public static final int menu_settings=0x7f080045;
+        public static final int menu_settings=0x7f080047;
+        public static final int menu_tasks=0x7f080045;
         public static final int new_session_menu=0x7f080043;
         public static final int none=0x7f080000;
         public static final int overview_sessions_adapter=0x7f080027;
@@ -290,11 +292,11 @@ containing a value of this type.
         /**  Activities 
          */
         public static final int activity_new_session=0x7f050002;
+        public static final int activity_new_task=0x7f050007;
         public static final int activity_sessions=0x7f050004;
         public static final int activity_settings=0x7f050005;
         public static final int activity_settings_activities=0x7f050006;
         public static final int activity_sign_up=0x7f050003;
-        public static final int activity_task=0x7f050007;
         public static final int activity_tasks=0x7f050008;
         public static final int app_name=0x7f050000;
         public static final int default_sessions_no=0x7f050027;
