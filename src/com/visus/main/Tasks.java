b7cbc5c16ac6b0b9460f8a99889e9a0fca21afc2
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-09-19 22:04:24 +0100

    Implemented no tasks method

--- src/com/visus/main/Tasks.java
+++ src/com/visus/main/Tasks.java
@@ -1,9 +1,11 @@
 package com.visus.main;
 
 import com.visus.R;
+import com.visus.database.TasksTableHandler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Tasks extends Activity {
 
@@ -12,6 +14,25 @@ public class Tasks extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_tasks);
+		
+		displayNoTasks();
+	}
+	
+	/**
+	 * Displays number of tasks available
+	 */
+	private void displayNoTasks() {
+		TasksTableHandler tasksHandler = new TasksTableHandler(this);
+		int count = tasksHandler.getCount();
+		tasksHandler.close();
+		
+		TextView txtNoTasks = (TextView) findViewById(R.id.tasks_no_tasks);
+		
+		if(count == 0) {
+			txtNoTasks.setText(0);
+		} else {
+			txtNoTasks.setText(count);
+		}	
 	}
 	
 }
