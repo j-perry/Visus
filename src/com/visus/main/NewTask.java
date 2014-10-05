b2af8bb744a0642c4e8c84d595077f83bd12b84e
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-10-05 17:51:52 +0100

    Set date functionality now works

--- src/com/visus/main/NewTask.java
+++ src/com/visus/main/NewTask.java
@@ -1,23 +1,52 @@
 package com.visus.main;
 
+import java.util.Calendar;
+
 import com.visus.R;
 import com.visus.database.TasksTableHandler;
 import com.visus.entities.Task;
 import com.visus.ui.main.fragments.LatestActivityFragment;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewTask extends Activity {
+	
+	private int day;
+	private int month;
+	private int year;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_new_task);
+		
+		Calendar cal = Calendar.getInstance();
+		day = cal.get(Calendar.DAY_OF_MONTH);
+		month = cal.get(Calendar.MONTH);
+		year = cal.get(Calendar.YEAR);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void setDate(View view) {
+		showDialog(999);
+	}
+	
+	@Override
+	public Dialog onCreateDialog(int id) {
+		if(id == 999) {
+			return new DatePickerDialog(this, myDateListener, year, month, day);
+		}
+		return null;
 	}
 	
 	/**
@@ -42,6 +71,29 @@ public class NewTask extends Activity {
 		
 		Intent intent = new Intent(this, Tasks.class);
 		startActivity(intent);
+	}
+		
+	/**
+	 * Create an event listener that displays a date picker
+	 */
+	private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+		
+		@Override
+		public void onDateSet(DatePicker view, int day, int month, int year) {
+			setDatePicker(day, month+1, year);
+		}
+	   
+	};
+	
+	/**
+	 * 
+	 * @param day
+	 * @param month
+	 * @param year
+	 */
+	private void setDatePicker(int day, int month, int year) {
+		TextView tvDate = (TextView) findViewById(com.visus.R.id.new_task_date);
+		tvDate.setText(day + "-" + month + "-" + year);
 	}
 	
 }
